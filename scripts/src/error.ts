4c392c8e772ba9c2e74a0201a6925ee60424bf01
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2025-08-26 15:14:38 -0400

    Fix ?

--- scripts/src/error.ts
+++ scripts/src/error.ts
@@ -1,3 +1,5 @@
+import path from 'node:path'
+
 type GitHubErrorOptions = {
   filename: string
   line: number
@@ -7,8 +9,12 @@ type GitHubErrorOptions = {
 
 export class GitHubError extends Error {
   constructor({ filename, line, title, message }: GitHubErrorOptions) {
+    // Convert the filename to a relative path
+    const rootPath = path.join(import.meta.dirname, '..', '..')
+    const relativeFilename = path.relative(rootPath, filename)
+
     // GitHub will display inline errors if we log the error in the following format
-    const errorMessage = `::error file=${filename},line=${line},title=${title}::${message}`
+    const errorMessage = `::error file=${relativeFilename},line=${line},title=${title}::${message}`
     super(errorMessage)
   }
 }
